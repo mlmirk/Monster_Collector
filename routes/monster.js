96c164d1037fa6b44f2ef649ea767c26c8498868
mlmirk <mike.l.mirkovic@gmail.com>
2022-05-03 13:07:38 -0700

    change order of routes

--- routes/monster.js
+++ routes/monster.js
@@ -3,8 +3,8 @@ const router = Router();
 import * as monsterController from "../controllers/monsters.js";
 import { isLoggedIn } from "../middleware/middleware.js";
 
-router.get("/", monsterController.index);
 router.get("/:id", monsterController.findOneMonster);
+router.get("/", monsterController.index);
 router.post("/", monsterController.create);
 router.put("/:id", monsterController.update);
 router.delete("/:id", monsterController.deleteMonster);
