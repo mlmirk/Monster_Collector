a9740ac0c61e195931fe816a0087f5904e93d617
mlmirk <mike.l.mirkovic@gmail.com>
2022-05-03 13:21:41 -0700

    change method to show ENDPOINT

--- routes/monster.js
+++ routes/monster.js
@@ -3,8 +3,9 @@ const router = Router();
 import * as monsterController from "../controllers/monsters.js";
 import { isLoggedIn } from "../middleware/middleware.js";
 
-router.get("/:id", monsterController.show);
+router.get("/:id/", monsterController.show);
 router.get("/", monsterController.index);
+
 router.post("/", monsterController.create);
 router.put("/:id", monsterController.update);
 router.delete("/:id", monsterController.deleteMonster);
