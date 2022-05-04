c18772cacdeae8ecddb0ef1170755e7d2046412e
mlmirk <mike.l.mirkovic@gmail.com>
2022-05-04 15:11:59 -0700

    change route style

--- routes/monster.js
+++ routes/monster.js
@@ -3,14 +3,13 @@ const router = Router();
 import * as monsterController from "../controllers/monsters.js";
 import { isLoggedIn } from "../middleware/middleware.js";
 
-
 // routers for CRUD operations
 
 router.get("/", monsterController.index);
 router.get("/:id", monsterController.show);
 router.get("/search/:search", monsterController.search);
 router.post("/", monsterController.create);
-router.put("/:id", monsterController.update);
+router.post("/:id", monsterController.update);
 router.delete("/:id", monsterController.deleteMonster);
 
 export { router };
