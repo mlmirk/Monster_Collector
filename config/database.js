701eb94d46a25d5944d8a28593697a617578c632
mlmirk <mike.l.mirkovic@gmail.com>
2022-05-05 13:15:49 -0700

    fix import in db

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose'
+const mongoose = require("mongoose");
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL);
 
 // database connection event
-db.on('connected', function () {
-  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`)
-})
+db.on("connected", function () {
+  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+});
